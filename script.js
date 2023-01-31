b54575a31474b29b616e271d0e10d49f06b32444
Lance Lopez <lancelopez46@gmail.com>
2023-01-31 19:45:44 +0800

    convert function Book to class

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ let myLibrary = [];
 const form = document.querySelector("form");
 const bookshelf = document.querySelector(".bookshelf");
 
-function Book(title, author, pages, progress) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.progress = progress;
+class Book {
+  constructor(title, author, pages, progress) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.progress = progress;
+  }
 }
 
 // default books
@@ -18,84 +20,84 @@ const Book3 = new Book("Don Quixote", "Miguel de Cervantes", 1077, "unread");
 myLibrary.push(Book1, Book2, Book3);
 
 function addBookToLibrary() {
-	form.addEventListener("submit", (e) => {
-		const Books = new Book(
-			document.querySelector("#title").value,
-			document.querySelector("#author").value,
-			document.querySelector("#pages").value,
-			document.querySelector("input[name='progress']:checked").value
-		);
-		myLibrary.push(Books);
+  form.addEventListener("submit", (e) => {
+    const Books = new Book(
+      document.querySelector("#title").value,
+      document.querySelector("#author").value,
+      document.querySelector("#pages").value,
+      document.querySelector("input[name='progress']:checked").value
+    );
+    myLibrary.push(Books);
 
-		document.querySelector("form").reset();
-		resetShelf();
-		displayBooks();
-		toggleModal();
-		e.preventDefault();
-	});
+    document.querySelector("form").reset();
+    resetShelf();
+    displayBooks();
+    toggleModal();
+    e.preventDefault();
+  });
 }
 
 function resetShelf() {
-	while (bookshelf.firstChild) {
-		bookshelf.removeChild(bookshelf.firstChild);
-	}
+  while (bookshelf.firstChild) {
+    bookshelf.removeChild(bookshelf.firstChild);
+  }
 }
 
 function displayBooks() {
-	for (i = 0; i < myLibrary.length; i++) {
-		initializeBook(
-			myLibrary[i].title,
-			myLibrary[i].author,
-			myLibrary[i].pages,
-			myLibrary[i].progress
-		);
-	}
+  for (i = 0; i < myLibrary.length; i++) {
+    initializeBook(
+      myLibrary[i].title,
+      myLibrary[i].author,
+      myLibrary[i].pages,
+      myLibrary[i].progress
+    );
+  }
 }
 
 // function to generate HTML elements
 function initializeBook(title, author, pages, progress) {
-	const bookBody = document.createElement("div");
-	bookBody.classList.add("book-info");
+  const bookBody = document.createElement("div");
+  bookBody.classList.add("book-info");
 
-	const bookTitle = document.createElement("span");
-	bookTitle.textContent = `"${title}"`;
+  const bookTitle = document.createElement("span");
+  bookTitle.textContent = `"${title}"`;
 
-	const bookAuthor = document.createElement("span");
-	bookAuthor.textContent = `writen by ${author}`;
+  const bookAuthor = document.createElement("span");
+  bookAuthor.textContent = `writen by ${author}`;
 
-	const bookPages = document.createElement("span");
-	bookPages.textContent = `${pages} pages`;
+  const bookPages = document.createElement("span");
+  bookPages.textContent = `${pages} pages`;
 
-	const btnStatus = document.createElement("button");
-	if (progress == "read") {
-		btnStatus.classList.add("read-book");
-	}
-	// add data-book-no attribute to buttons
-	// used to remove object on myLibrary[${}]
-	btnStatus.setAttribute("data-book-no", `${i}`);
-	btnStatus.textContent = `${progress}`;
+  const btnStatus = document.createElement("button");
+  if (progress == "read") {
+    btnStatus.classList.add("read-book");
+  }
+  // add data-book-no attribute to buttons
+  // used to remove object on myLibrary[${}]
+  btnStatus.setAttribute("data-book-no", `${i}`);
+  btnStatus.textContent = `${progress}`;
 
-	const btnRemove = document.createElement("button");
-	btnRemove.classList.add("red-button");
-	btnRemove.setAttribute("data-book-no", `${i}`);
+  const btnRemove = document.createElement("button");
+  btnRemove.classList.add("red-button");
+  btnRemove.setAttribute("data-book-no", `${i}`);
 
-	btnRemove.textContent = "Remove";
+  btnRemove.textContent = "Remove";
 
-	bookshelf.append(bookBody);
-	bookBody.append(bookTitle, bookAuthor, bookPages, btnStatus, btnRemove);
+  bookshelf.append(bookBody);
+  bookBody.append(bookTitle, bookAuthor, bookPages, btnStatus, btnRemove);
 
-	btnStatus.addEventListener("click", (e) => {
-		btnStatus.classList.toggle("read-book");
-		return btnStatus.textContent == "read"
-			? (btnStatus.textContent = "unread")
-			: (btnStatus.textContent = "read");
-	});
+  btnStatus.addEventListener("click", (e) => {
+    btnStatus.classList.toggle("read-book");
+    return btnStatus.textContent == "read"
+      ? (btnStatus.textContent = "unread")
+      : (btnStatus.textContent = "read");
+  });
 
-	btnRemove.addEventListener("click", (e) => {
-		myLibrary.splice(e.target.dataset.bookNo, 1);
-		resetShelf();
-		displayBooks();
-	});
+  btnRemove.addEventListener("click", (e) => {
+    myLibrary.splice(e.target.dataset.bookNo, 1);
+    resetShelf();
+    displayBooks();
+  });
 }
 
 displayBooks();
@@ -106,13 +108,13 @@ const trigger = document.querySelector("#trigger");
 const modal = document.querySelector("#modal");
 
 function toggleModal() {
-	modal.classList.toggle("show-modal");
+  modal.classList.toggle("show-modal");
 }
 
 function windowOnClick(event) {
-	if (event.target === modal) {
-		toggleModal();
-	}
+  if (event.target === modal) {
+    toggleModal();
+  }
 }
 
 trigger.addEventListener("click", toggleModal);
